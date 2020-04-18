04d8767dfdcc7dde58866dbef4a73779bbf8ae3e
MB <mike@havenlight.net>
2020-04-17 17:56:54 -0700

    Browser test file.

---
+++ tests/browser.spec.ts
@@ -0,0 +1,38 @@
+import { ArmorBrowser } from '../src/browser';
+import { ArmorBrowserWindow } from '../src/window/window';
+import { EventEmitter } from 'events';
+
+describe('ArmorBrowser', () => {
+	let instance: ArmorBrowser;
+	let events: EventEmitter;
+
+	beforeAll(() => {
+		events = new EventEmitter();
+		instance = new ArmorBrowser(events);
+	});
+
+	describe('Constructor', () => {
+		it('should not throw when events argument is omitted', () => {
+			expect(() => {
+				const custom = new ArmorBrowser();
+			}).not.toThrow();
+		});
+
+		it('should create a new event emitter when events argument is omitted', () => {
+			const custom = new ArmorBrowser();
+			expect((custom.events instanceof EventEmitter)).toBe(true);
+		});
+
+		it('should initialize the windows property to an array', () => {
+			const custom = new ArmorBrowser();
+			expect(Array.isArray(custom.windows)).toBe(true);
+		});
+
+		it('should create one initial window and add it to the windows array', () => {
+			const custom = new ArmorBrowser();
+			expect(custom.windows).toHaveLength(1);
+			const wnd = custom.windows[0];
+			expect((wnd instanceof ArmorBrowserWindow)).toBe(true);
+		});
+	});
+});
