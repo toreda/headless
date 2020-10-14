728d988b50cd1aa3b8e2119b44345abecf84bafe
SaxanLosterToreda <70411010+SaxanLosterToreda@users.noreply.github.com>
2020-10-14 14:17:05 -0700

    Update dependicies

--- tests/browser.spec.ts
+++ tests/browser.spec.ts
@@ -3,7 +3,7 @@ import {BrowserRequest} from '../src/browser/request';
 import {BrowserRequestOptions} from '../src/browser/request/options';
 import {EventEmitter} from 'events';
 
-const htmltests = !false;
+const htmltests = false;
 
 describe('Browser', () => {
 	let instance: Browser;
